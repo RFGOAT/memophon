b007f2bb4658d1c9a39f2f47886e7df8f1829fc1
RFGOAT <41563520+RFGOAT@users.noreply.github.com>
2023-12-25 12:53:54 +0100

    Update DiscCounter.py

--- DiscCounter.py
+++ DiscCounter.py
@@ -1,2 +1,10 @@
-DiscCounter
-kate test
+import RPi.GPIO as GPIO
+
+BUTTON_GPIO = 16
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+while True:
+    GPIO.wait_for_edge(BUTTON_GPIO, GPIO.FALLING)
+    print("Button pressed!")
